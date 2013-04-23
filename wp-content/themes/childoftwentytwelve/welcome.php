1a6107332e598dec38197bafb30b3b1e4b465d94
webjen <jryden@outlook.com>
2013-04-23 19:04:42 +0200

    Created file for welcome message to be displayed on front index page

---
+++ wp-content/themes/childoftwentytwelve/welcome.php
@@ -0,0 +1,15 @@
+<!-- Welcome message to be displayed on the front index page -->
+
+<div id="front-page-intro" class="clear">
+
+  <article id="intro-elements">
+
+    <div class="entry-content">
+
+      <p>This is the text that is displayed by the welcome.php-file.</p>
+
+    </div> <!-- .entry-content -->
+
+  </article> <!-- #intro-elements -->
+
+</div> <!-- #front-page-intro -->
